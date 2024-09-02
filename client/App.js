f8ad19e752d1655f0c546f6f65301f1155a08452
Inderprit Singh <singhinderprit1992@gmail.com>
2024-09-02 17:39:30 -0500

    fixing up file structure

--- client/App.js
+++ client/App.js
@@ -1,31 +1,39 @@
-import React, { useState } from "react";
-import axios from "axios";
+// import React, { useState } from "react";
+// import axios from "axios";
+
+// export const App = () => {
+//   const [text, setText] = useState("");
+
+//   const onSubmitHandler = async (e) => {
+//     e.preventDefault();
+//     const data = await axios.post("/postData", { text: text });
+//   };
+
+//   const onChangeHandler = (e) => {
+//     setText(e.target.value);
+//   };
+
+//   return (
+//     <div>
+//       <h1>App Component</h1>
+//       <input type="text" onChange={(e) => onChangeHandler(e)} />
+//       <button type="submit" onClick={(e) => onSubmitHandler(e)}>
+//         Click Me
+//       </button>
+//     </div>
+//   );
+// };
+
+import React from "react";
+import TestForm from "./Components/TestForm";
 
 export const App = () => {
-  const [text, setText] = useState("");
-
-  const onSubmitHandler = async (e) => {
-    e.preventDefault();
-    console.log(`Text is here ${text}`);
-    const data = await axios.post("/postData", { text: text });
-    console.log(data, "here is data");
-    console.log(`Submit Handler Was Clicked`);
-  };
-
-  const onChangeHandler = (e) => {
-    setText(e.target.value);
-    //console.log(text, "here is text");
-  };
-
-  console.log(`Here is text ${text}`);
-
   return (
     <div>
       <h1>App Component</h1>
-      <input type="text" onChange={(e) => onChangeHandler(e)} />
-      <button type="submit" onClick={(e) => onSubmitHandler(e)}>
-        Click Me
-      </button>
+      <h3>
+        <TestForm />
+      </h3>
     </div>
   );
 };
---
+++ client/Components/TestForm.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class TestForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div>I Am TestForm</div>;
+  }
+}
+
+export default TestForm;
